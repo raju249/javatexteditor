05adbdbf9ba0db303a650c6e3439e17684fdd521
Rajendra <rajendrakadam249@gmail.com>
2016-05-23 20:08:36 +0530

    AutoCorrect implementation for final course

--- src/spelling/NearbyWords.java
+++ src/spelling/NearbyWords.java
@@ -76,7 +76,23 @@ public class NearbyWords implements SpellingSuggest {
 	 * @return
 	 */
 	public void insertions(String s, List<String> currentList, boolean wordsOnly ) {
-		// TODO: Implement this method  
+		// TODO: Implement this method 
+		for(int index = 0; index <= s.length(); index++){
+			for(int charCode = (int)'a'; charCode <= (int)'z'; charCode++) {
+				// use StringBuffer for an easy interface to permuting the 
+				// letters in the String
+				StringBuffer sb = new StringBuffer(s);
+				sb.insert(index, (char)charCode);
+
+				// if the item isn't in the list, isn't the original string, and
+				// (if wordsOnly is true) is a real word, add to the list
+				if(!currentList.contains(sb.toString()) && 
+						(!wordsOnly||dict.isWord(sb.toString())) &&
+						!s.equals(sb.toString())) {
+					currentList.add(sb.toString());
+				}
+			}
+		}
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
@@ -88,6 +104,19 @@ public class NearbyWords implements SpellingSuggest {
 	 */
 	public void deletions(String s, List<String> currentList, boolean wordsOnly ) {
 		// TODO: Implement this method
+		for(int index = 0; index < s.length(); index++){
+			
+				StringBuffer sb = new StringBuffer(s);
+				sb.deleteCharAt(index);
+
+				
+				if(!currentList.contains(sb.toString()) && 
+						(!wordsOnly||dict.isWord(sb.toString())) &&
+						!s.equals(sb.toString())) {
+					currentList.add(sb.toString());
+				}
+			
+		}
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
@@ -111,13 +140,29 @@ public class NearbyWords implements SpellingSuggest {
 		visited.add(word);
 					
 		// TODO: Implement the remainder of this method, see assignment for algorithm
-		
+		int count = 0;
+		int threshold = 0;
+		while (!queue.isEmpty() && count < numSuggestions && threshold < THRESHOLD){
+			String current = queue.remove(0);
+			List <String> distanceOne = distanceOne(current,true);
+			for (String s : distanceOne){
+				if(!visited.contains(s)){
+					queue.add(s);
+					visited.add(s);
+					if (dict.isWord(s)){
+						retList.add(s);
+						count++;
+					}
+				}
+			}
+			threshold++;
+		}
 		return retList;
 
 	}	
 
    public static void main(String[] args) {
-	   /* basic testing code to get started
+	    /*basic testing code to get started
 	   String word = "i";
 	   // Pass NearbyWords any Dictionary implementation you prefer
 	   Dictionary d = new DictionaryHashSet();
